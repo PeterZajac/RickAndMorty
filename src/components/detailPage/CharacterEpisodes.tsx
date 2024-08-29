a2a9bbcb7ec2962bca4fd778586f85f5b02d309d
PeterZajac <zajacp011@gmail.com>
2024-08-29 15:03:31 +0200

    style: Update CharacterEpisodes component to use CSS variables for colors

--- src/components/detailPage/CharacterEpisodes.tsx
+++ src/components/detailPage/CharacterEpisodes.tsx
@@ -1,49 +1,13 @@
 const CharacterEpisodes = ({ episodesData }: { episodesData: string[] }) => {
   return (
     <div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          marginTop: '28px',
-        }}
-      >
-        <h3
-          style={{
-            fontSize: '12px',
-            fontWeight: '700',
-            fontFamily: 'Montserrat , sans-serif',
-            textTransform: 'uppercase',
-            letterSpacing: '1.5px',
-          }}
-        >
+      <div className='character-episodes-info'>
+        <h3 className='character-episodes-h3'>
           Episodes{' '}
-          <span
-            style={{
-              backgroundColor: '#0D8CD2',
-              color: 'white',
-              borderRadius: '5px',
-              fontSize: '11px',
-              width: '23px',
-              height: '23px',
-              display: 'inline-block',
-              textAlign: 'center',
-              lineHeight: '23px',
-            }}
-          >
-            {episodesData.length}
-          </span>
+          <span className='character-episodes-span'>{episodesData.length}</span>
         </h3>
       </div>
-      <div
-        style={{
-          fontSize: '14px',
-          fontWeight: '400',
-          fontFamily: 'Montserrat , sans-serif',
-          width: '611px',
-          marginTop: '12px',
-        }}
-      >
+      <div className='character-episodes-names '>
         {' '}
         {episodesData.join(', ')}
       </div>
